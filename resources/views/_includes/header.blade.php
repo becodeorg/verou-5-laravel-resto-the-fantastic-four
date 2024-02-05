2828f4dd4ada4569d8c54bc1c152a2efff57165c
alex <vootehdoo@gmail.com>
2024-02-05 13:09:37 +0100

    Add basic header + layouts and includes folder

---
+++ resources/views/_includes/header.blade.php
@@ -0,0 +1,11 @@
+<header>
+    <div class="flex bg-gray-800 text-white top-0 py-3 flex-wrap justify-around bg-silver">
+        <h1 class="text-lg font-semibold">The Fantastic Four</h1>
+        <ul class="flex gap-[40px] text-m">
+            <li><a href="">Home</a></li>
+            <li><a href="">Menu</a></li>
+            <li><a href="">Book table</a></li>
+            <li><a href="">Contact</a></li>
+        </ul>
+    </div>
+</header>
---
+++ resources/views/_layouts/layout.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title')</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+
+<body>
+    @include('./_includes/header')
+    @yield('content')
+</body>
+
+</html>
