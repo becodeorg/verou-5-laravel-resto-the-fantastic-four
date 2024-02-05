32921e7bd89219bc1f29dcb279614c1459011ba1
Thierry De Belder <thierrydebelder@Laptop-van-Thierry.local>
2024-02-05 15:31:10 +0100

    add route to button and header

--- resources/views/_includes/header.blade.php
+++ resources/views/_includes/header.blade.php
@@ -4,7 +4,7 @@
         <ul class="flex gap-[40px] text-m">
             <li><a href="/">Home</a></li>
             <li><a href="">Menu</a></li>
-            <li><a href="">Book table</a></li>
+            <li><a href="{{route("reservation")}}">Book table</a></li>
             <li><a href="">Contact</a></li>
             @auth
                 <li><a href="{{ route('logout') }}">Logout</a></li>
@@ -12,3 +12,5 @@
         </ul>
     </div>
 </header>
+
+
