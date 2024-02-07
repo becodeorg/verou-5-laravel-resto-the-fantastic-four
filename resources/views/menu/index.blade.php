4b685e1c971777761cafa08a45c60a9b3a1ffb36
Thierry De Belder <thierrydebelder@Laptop-van-Thierry.local>
2024-02-07 15:09:50 +0100

    Change regular cards to foreach on menu page

--- resources/views/menu/index.blade.php
+++ resources/views/menu/index.blade.php
@@ -4,6 +4,43 @@
 @endsection
 
 @section('content')
+
+  {{-- <select name="Appetizers" id="Appetizers">
+  @foreach($Appetizers as $Appetizer)
+    <option value="{{$table}}" >table {{$table->id}} (seats {{$table->places}})</option>
+  @endforeach
+  </select> --}}
+
+  <div class="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+    <h1 class="mt-24 text-4xl text-center font-bold tracking-tight text-white sm:mt-10 sm:text-6xl">Our Menu</h1> 
+    <h2 class="mt-24 text-4xl text-center font-bold tracking-tight text-white sm:mt-10 sm:text-2xl">Appetizers</h2><br><br>
+    <div class="grid grid-cols-1 gap-y-4 sm:grid-cols-2 sm:gap-x-6 sm:gap-y-10 lg:grid-cols-4 lg:gap-x-8">
+        @foreach($Appetizers as $Appetizer)
+        <div class="group relative flex flex-col overflow-hidden rounded-lg border border-gray-200 bg-white">
+            <div class="aspect-h-4 aspect-w-3 bg-gray-200 sm:aspect-none group-hover:opacity-75 sm:h-96">
+                <img src="{{ asset('images/'.$Appetizer->image) }}"
+                    alt="{{ $Appetizer->name }}"
+                    class="h-full w-full object-cover object-center sm:h-full sm:w-full">
+            </div>
+            <div class="flex flex-1 flex-col space-y-2 p-4">
+                <h3 class="text-sm font-medium text-gray-900">
+                    <a href="#">
+                        <span aria-hidden="true" class="absolute inset-0"></span>
+                        {{ $Appetizer->name }}
+                    </a>
+                </h3>
+                <p class="text-sm text-gray-500">{{ $Appetizer->description }}</p>
+                <div class="flex flex-1 flex-col justify-end">
+                    <p class="text-sm italic text-gray-500">{{ $Appetizer->tag }}</p>
+                    <p class="text-base font-medium text-gray-900">${{ $Appetizer->price }}</p>
+                </div>
+            </div>
+        </div>
+        @endforeach
+    </div>
+</div>
+
+
     <div class="bg-gray-900">
         <div class="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
             <h1 class="mt-24 text-4xl text-center font-bold tracking-tight text-white sm:mt-10 sm:text-6xl">Our Menu</h1>
